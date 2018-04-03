0e0eda2e912a4c770bf3c1f8d5670a01891679b0
Foma555 <Foma1553@mail.ru>
2018-04-04 00:16:00 +0300

    Визитка

--- guessnumber/scrypt.js
+++ guessnumber/scrypt.js
@@ -1,6 +1,9 @@
 //Переменные игры
 var mysteryNumber = 50;
 var playersGuess = 0;
+var guessesRemaining = 10;
+var guessesMade = 0;
+var gameState = "";
 //Поля ввода и вывода
 var input = document.querySelector("#input");
 var output = document.querySelector("#output");
@@ -14,15 +17,18 @@ playGame();
 }
 function playGame()
 {
+	guessesRemaining -= 1;
+    guessesMade += 1;
+    gameState = " Сделано попыток: " + guessesMade + ". Осталось: " + guessesRemaining + ".";
 	playersGuess = parseInt(input.value);
 	console.log(playersGuess);
 	if(playersGuess > mysteryNumber)
 	{
-		output.innerHTML = "Слишком большое";
+		output.innerHTML = "Слишком большое." + gameState;
 	}
 	else if(playersGuess < mysteryNumber)
 	{
-		output.innerHTML = "Слишком маленькое";
+		output.innerHTML = "Слишком маленькое" + gameState;
 	}
 	else if (playersGuess === mysteryNumber)
 	{
@@ -30,6 +36,6 @@ function playGame()
 	}
 	else
 	{
-		output.innerHTML = "Вы ввели хрень!";
+		output.innerHTML = "Вы ввели хрень!" + gameState;
 	}	
 }
